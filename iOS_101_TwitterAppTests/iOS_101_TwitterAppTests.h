5d6cf64b2482b09f7cad6329755e9e2a13544401
Tomasz Janeczko <janecztom@gmail.com>
2013-03-22 21:46:08 +0100

    Initial commit

---
+++ iOS_101_TwitterAppTests/iOS_101_TwitterAppTests.h
@@ -0,0 +1,13 @@
+//
+//  iOS_101_TwitterAppTests.h
+//  iOS_101_TwitterAppTests
+//
+//  Created by Tomasz Janeczko on 03/17/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface iOS_101_TwitterAppTests : SenTestCase
+
+@end
---
+++ iOS_101_TwitterAppTests/iOS_101_TwitterAppTests.m
@@ -0,0 +1,32 @@
+//
+//  iOS_101_TwitterAppTests.m
+//  iOS_101_TwitterAppTests
+//
+//  Created by Tomasz Janeczko on 03/17/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "iOS_101_TwitterAppTests.h"
+
+@implementation iOS_101_TwitterAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in iOS_101_TwitterAppTests");
+}
+
+@end
